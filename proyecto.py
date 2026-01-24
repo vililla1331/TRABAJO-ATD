a2e4fe0b5fc020958dcfb1b6e0460b815336b593
dcg222 <130308517+dcg222@users.noreply.github.com>
2026-01-24 18:45:08 +0100

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -73,3 +73,53 @@ def obtener_nota(lista_titulos):
     try: driver.quit()
     except: pass
     return dic_nota,dic_actores
+    
+#PAGINA 3: JUSTWACH: buscar las plataformas de cada película
+def buscar_plataformas(dic_nota):
+    driver = webdriver.Chrome()
+    dic_resultados = {}
+    print(f"Iniciando una búsqueda para {len(dic_nota)} películas en JustWatch...")
+    #Gestión de las Cookies
+    driver.get("https://www.justwatch.com/es")
+    time.sleep(3)
+    try:
+        botones = driver.find_elements(By.TAG_NAME, "button")
+        for boton in botones:
+            if "Aceptar" in boton.text or "Accept" in boton.text:
+                boton.click()
+                print("Cookies aceptadas.")
+                break
+    except:
+        print("No se pudieron aceptar las cookies o no aparecieron.")
+    #Búsqueda de plataforma para cada película
+    for pelicula in dic_nota.keys():
+        print(f"Buscando: {pelicula}...")
+        url_busqueda = f"https://www.justwatch.com/es/buscar?q={pelicula.replace(' ', '%20')}"
+        driver.get(url_busqueda)
+        time.sleep(3)
+        try:
+            #Buscamos el primer elemento de la rejilla de resultados al buscar la pelicula que contiene "title-list-grid__item"
+            primer_resultado = driver.find_element(By.CSS_SELECTOR, "div.title-list-grid__item a")
+            primer_resultado.click()
+
+            time.sleep(3)
+            
+            #Las plataformas están dentro de una caja llamada "buybox-row__offers" como imágenes
+            imagenes_plataformas = driver.find_elements(By.CSS_SELECTOR, "div.buybox-row__offers img")
+            plataformas= []
+            for imagen in imagenes_plataformas:
+                nombre_plataforma = imagen.get_attribute("alt")
+                if nombre_plataforma not in plataformas:
+                    plataformas.append(nombre_plataforma)
+            #Guardamos en el diccionario el resultado:
+            dic_resultados[pelicula] = plataformas
+            print(f"Película encontrada en: {plataformas}")
+        except Exception as e:
+            # Si falla (no hay plataformas encontradas), guardamos una lista vacía
+            print(f"No se encontraron plataformas para la película o hubo un error: {e}")
+            dic_resultados[pelicula] = ["No disponible, error"]
+            continue
+
+    driver.quit()
+    
+    return dic_resultados
